8bbe96d11f4cd8225fd397c5cffe25668b8717f3
Allan Germé <allan.germe@outlook.fr>
2021-02-04 18:30:56 +0100

    Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './Scss/Sass/Components/_header.scss';
 import './Scss/Sass/Components/Desc.scss';
 import './Scss/Sass/Components/Meteo.scss';
+import './Scss/Sass/Components/_footer.scss';
+
 import Router from "./Components/Router"
 
 function App() {
